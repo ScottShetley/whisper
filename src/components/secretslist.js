8f4763c54f51980152d74206781c319853bc5737
ScottShetley <davidscottshetley@gmail.com>
2018-10-03 15:01:51 -0700

    updated shop.js

---
+++ src/components/secretslist.js
@@ -0,0 +1,11 @@
+export let secrets = [
+  { id: 1, title: "Ammon's middle name", price: 200 },
+  { id: 2, title: "Scott's real identity", price: 100 },
+  { id: 3, title: "Vinny's address", price: 5 },
+  { id: 4, title: "Sam's hacker name", price: 900 },
+  {
+    id: 5,
+    title: "Ramy's favorite food, Clinton's favorite video game",
+    price: 9001
+  }
+];
--- src/components/shop.js
+++ src/components/shop.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Shop = props => <div>Shop</div>;
+const mapStateToProps = state => {
+  return {
+    secrets: state.secrets
+  };
+};
 
-export default Shop;
+const Shop = props => (
+  <div>
+    <h1 className="cover-heading">Secrets...........</h1>
+    <ul>
+      {props.secrets.map(s => (
+        <li key={s.id}>
+          {s.title}
+          <button className="btn btn-default">${s.price}</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default connect(mapStateToProps)(Shop);
